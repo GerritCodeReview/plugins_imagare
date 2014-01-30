6237f7634ca6c130b0973fcd5b54bffa68d5bcf2
Edwin Kempin <edwin.kempin@sap.com>
2014-01-30 23:42:38 +0100

    Support uploading images from file system
    Change-Id: I2969c2dd33294fff0667b8034df452f084a88c03
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/imagare/client/UploadByFileSelection.java
@@ -0,0 +1,50 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.imagare.client;
+
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+public class UploadByFileSelection extends HorizontalPanel {
+  UploadByFileSelection() {
+    this("All-Projects");
+  }
+
+  UploadByFileSelection(String project) {
+    init0();
+
+    FormPanel form = new FormPanel();
+    FileUpload upload = new FileUpload();
+    upload.setName("Select Image");
+    upload.getElement().setAttribute("onChange", "imagareSubmit(event)");
+    form.add(upload);
+
+    add(form);
+  }
+
+  private static native void init0() /*-{
+    $wnd.imagareSubmit = function submit(event) {
+      var f = event.target.files[0];
+      if (f) {
+        var r = new FileReader();
+        r.onload = function(e) {
+          @com.googlesource.gerrit.plugins.imagare.client.ImageUploader::uploadImage(Ljava/lang/String;Ljava/lang/String;)(e.target.result, f.name);
+        }
+        r.readAsDataURL(f);
+      }
+    }
+  }-*/;
+}

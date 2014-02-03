e158a70c3ae64647c307ba421f54c9be4a6931eb
Edwin Kempin <edwin.kempin@sap.com>
2014-02-03 18:00:29 +0100

    Allow deletion of just uploaded images from WebUI
    Change-Id: I793eea5754dbe3ab99301ad9fde66ec590ff202f
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

--- src/main/java/com/googlesource/gerrit/plugins/imagare/client/UploadedImagesPanel.java
+++ src/main/java/com/googlesource/gerrit/plugins/imagare/client/UploadedImagesPanel.java
@@ -14,6 +14,9 @@
 
 package com.googlesource.gerrit.plugins.imagare.client;
 
+import com.google.gerrit.plugin.client.Plugin;
+import com.google.gerrit.plugin.client.rpc.NoContent;
+import com.google.gerrit.plugin.client.rpc.RestApi;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.MouseOutEvent;
@@ -22,12 +25,14 @@ import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwtexpui.clippy.client.CopyableLabel;
+import com.google.gwtexpui.safehtml.client.SafeHtmlBuilder;
 
 public class UploadedImagesPanel extends FlowPanel {
 
@@ -55,12 +60,18 @@ public class UploadedImagesPanel extends FlowPanel {
     private final Image img;
     private final Image fullScreenIcon;
     private Timer fullScreenIconHideTimer;
+    private final Image deleteIcon;
+    private Timer deleteIconHideTimer;
 
     ImagePreview(final String url) {
       setStyleName("imagare-uploaded-image-preview-panel");
 
-      String fileName = url.substring(url.indexOf('/', url.indexOf("/rev/") + 5) + 1);
-      fileName = fileName.replaceAll("\\+", " ");
+      int revIndex = url.indexOf("/rev/");
+      int projectIndex = url.lastIndexOf('/', revIndex - 1);
+      int fileIndex = url.indexOf('/', revIndex + 5);
+      final String project = url.substring(projectIndex + 1, revIndex);
+      final String ref = url.substring(revIndex + 5, fileIndex);
+      final String fileName = url.substring(fileIndex + 1).replaceAll("\\+", " ");
       Label fileNameLabel = new Label(fileName);
       fileNameLabel.setStyleName("imagare-uploaded-image-title");
       add(fileNameLabel);
@@ -74,6 +85,12 @@ public class UploadedImagesPanel extends FlowPanel {
       fullScreenIcon.setTitle("Full Screen");
       fullScreenIcon.setVisible(false);
       add(fullScreenIcon);
+
+      deleteIcon = new Image(ImagarePlugin.RESOURCES.delete());
+      deleteIcon.setStyleName("imagare-delete-icon");
+      deleteIcon.setTitle("Delete Image");
+      deleteIcon.setVisible(false);
+      add(deleteIcon);
 
       img.addMouseOverHandler(new MouseOverHandler() {
         @Override
@@ -97,6 +114,13 @@ public class UploadedImagesPanel extends FlowPanel {
                   + "position: absolute; top: " + img.getAbsoluteTop() + "px; "
                   + "left: " + img.getAbsoluteLeft() + "px;");
           fullScreenIcon.setVisible(true);
+
+          cancelHideDeleteIcon();
+          deleteIcon.getElement().setAttribute("style",
+              deleteIcon.getElement().getAttribute("style")
+                  + "position: absolute; top: " + img.getAbsoluteTop() + "px; "
+                  + "left: " + (img.getAbsoluteLeft() + fullScreenIcon.getWidth()) + "px;");
+          deleteIcon.setVisible(true);
         }
       });
       img.addMouseOutHandler(new MouseOutHandler() {
@@ -105,6 +129,7 @@ public class UploadedImagesPanel extends FlowPanel {
           popup.setVisible(false);
           popup.clear();
           hideFullScreenIcon();
+          hideDeleteIcon();
         }
       });
 
@@ -112,6 +137,7 @@ public class UploadedImagesPanel extends FlowPanel {
         @Override
         public void onMouseOver(MouseOverEvent event) {
           cancelHideFullScreenIcon();
+          cancelHideDeleteIcon();
         }
       });
 
@@ -119,6 +145,7 @@ public class UploadedImagesPanel extends FlowPanel {
         @Override
         public void onMouseOut(MouseOutEvent event) {
           hideFullScreenIcon();
+          hideDeleteIcon();
         }
       });
 
@@ -128,6 +155,53 @@ public class UploadedImagesPanel extends FlowPanel {
           Window.open(url, "_blank", "");
           popup.setVisible(false);
           popup.clear();
+        }
+      });
+
+      deleteIcon.addMouseOverHandler(new MouseOverHandler() {
+        @Override
+        public void onMouseOver(MouseOverEvent event) {
+          cancelHideFullScreenIcon();
+          cancelHideDeleteIcon();
+        }
+      });
+
+      deleteIcon.addMouseOutHandler(new MouseOutHandler() {
+        @Override
+        public void onMouseOut(MouseOutEvent event) {
+          hideDeleteIcon();
+          hideFullScreenIcon();
+        }
+      });
+
+      deleteIcon.addClickHandler(new ClickHandler() {
+        @Override
+        public void onClick(ClickEvent event) {
+          new ConfirmationDialog("Delete Image",
+              new SafeHtmlBuilder().append("Are you sure you want to delete '" + fileName + "'?").br().br(),
+              new ConfirmationCallback() {
+                @Override
+                public void onOk() {
+                  new RestApi("projects").view(project)
+                      .view(Plugin.get().getPluginName(), "images").view(ref)
+                      .delete(new AsyncCallback<NoContent>() {
+                        @Override
+                        public void onSuccess(NoContent info) {
+                          UploadedImagesPanel.this.remove(ImagePreview.this);
+                          UploadedImagesPanel.this.setVisible(
+                              UploadedImagesPanel.this.getWidgetCount() > 1);
+
+                          popup.setVisible(false);
+                          popup.clear();
+                        }
+
+                        @Override
+                        public void onFailure(Throwable caught) {
+                          // never invoked
+                        }
+                      });
+                }
+              }).center();
         }
       });
 
@@ -150,6 +224,23 @@ public class UploadedImagesPanel extends FlowPanel {
       if (fullScreenIconHideTimer != null) {
         fullScreenIconHideTimer.cancel();
         fullScreenIconHideTimer = null;
+      }
+    }
+
+    private void hideDeleteIcon() {
+      deleteIconHideTimer = new Timer() {
+        @Override
+        public void run() {
+          deleteIcon.setVisible(false);
+        }
+      };
+      deleteIconHideTimer.schedule(20);
+    }
+
+    private void cancelHideDeleteIcon() {
+      if (deleteIconHideTimer != null) {
+        deleteIconHideTimer.cancel();
+        deleteIconHideTimer = null;
       }
     }
   }
